c1c2bfe1bd47ddb0cf754f96a4635472b5fd8197
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-09-21 22:20:21 +0900

    action 생성기와 리듀서

--- 1.redux/index.js
+++ 1.redux/index.js
@@ -5,7 +5,30 @@ const initialState = {
     compB: 12,
     compC: null,
 }
-const reducer = () => {}
+const reducer = (preState, action) => {
+  switch(action.type){
+    case 'CHANGE_COMP_A':
+        return {
+            compA: action.data,
+            compB: 12,
+            compC: null
+        }
+    case 'CHANGE_COMP_B':
+        return {
+            compa: 'a',
+            compB: action.data,
+            compC: null
+        }
+  }
+}
 const store = createStore(reducer, initialState);
 
+const changeCompA = (data) => {
+  return {
+    type: 'CHANGE_COMP_A',
+    data,
+  }
+}
+
+store.dispatch(changeCompA('b'))
 console.log(store.getState());
